3dee003b128bd145c931a5a30a7a8ca39adede91
orlanth23 <orlanth23@hotmail.com>
2022-04-29 13:08:43 +1100

    CI

--- build_env_prod.sh
+++ build_env_prod.sh
@@ -3,13 +3,13 @@
 # Generate environment_config.dart with Production variables.
 # Author : OLIVE Jean-Paul
 
-if [ -z "$TEAMATE_MANAGER_SERVER_URL" ]
+if [ -z $TEAMATE_MANAGER_SERVER_URL ]
 then
   echo "The variable TEAMATE_MANAGER_SERVER_URL couldn't be null"
   exit 1
 fi
 
-if [ -z "$TEAMATE_MANAGER_RESOURCE_STORAGE" ]
+if [ -z $TEAMATE_MANAGER_RESOURCE_STORAGE ]
 then
   echo "The variable TEAMATE_MANAGER_RESOURCE_STORAGE couldn't be null"
   exit 1
